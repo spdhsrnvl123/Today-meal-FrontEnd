a3c96f965fd3427bf3f747367a56617d09588780
이태형 <spdhsrnvl456@gmail.com>
2024-02-15 21:11:24 +0900

    ✨feat:카카오지도 인포윈도우 추가

--- src/app/services/kakao-api/kakao-api.component.ts
+++ src/app/services/kakao-api/kakao-api.component.ts
@@ -35,7 +35,7 @@ export class KakaoAPIComponent implements OnInit {
     this.mapOption = mapOption;
 
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-    this.map = new kakao.maps.Map(mapContainer, mapOption);
+      this.map = new kakao.maps.Map(mapContainer, mapOption);
 
     this.dataStatusSubscription = this.kakaoMapApiService.dataStatusSubject.subscribe((newData)=>{
       this.displayPlaces(newData)
@@ -50,36 +50,49 @@ export class KakaoAPIComponent implements OnInit {
         level: 3 // 지도의 확대 레벨
       };
 
-    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
+    let map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
     // -------------------------------------------------------------------------------
-
-
 
     let bounds = new kakao.maps.LatLngBounds()
     let imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png";
     // 마커 이미지의 이미지 크기 입니다
     let imageSize = new kakao.maps.Size(36, 37);
     // let imgOptions =  {
-    //     spriteSize : new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+    //     spriteSize : newㅏㅑ   kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
     //     spriteOrigin : new kakao.maps.Point(0, (idx*46)+10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
     //     offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
     // }
     // let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions);
 
     places.forEach((place,idx) => {
-      let imgOptions =  {
-        spriteSize : new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-        spriteOrigin : new kakao.maps.Point(0, (idx*46)+10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
-        offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
-      }
-      let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions);
-      new kakao.maps.Marker({
+      // 인포 윈도우 생성
+      let iwContent = `<div style="padding:5px;">(${idx+1}) ${place.place_name}</div>` // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+      let iwPosition = new kakao.maps.LatLng(place.y, place.x); //인포윈도우 표시 위치입니다
+
+      let infowindow = new kakao.maps.InfoWindow({
+        position: iwPosition,
+        content: iwContent,
+      });
+
+      console.log(place);
+      let imgOptions = {
+        spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+        spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+        offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+      };
+      let markerImage = new kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imgOptions
+      );
+      let marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: new kakao.maps.LatLng(place.y, place.x), // 마커를 표시할 위치
         // title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-        image : markerImage // 마커 이미지
+        image: markerImage, // 마커 이미지
       });
+      // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+      infowindow.open(map, marker);
     });
     map.setCenter(new kakao.maps.LatLng(places[0].y, places[0].x));
 
