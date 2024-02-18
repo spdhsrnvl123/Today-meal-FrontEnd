c4a739ed1e220f0e7582d26f1af77832cfdaa1b4
LeutBing <spdhsrnvl456@gmail.com>
2024-02-18 21:02:54 +0900

    ✨feat:순위1위 표시 컴포넌트 구현

---
+++ src/app/layouts/rank-top/rank-top.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rank-top',
+  templateUrl: './rank-top.component.html',
+  styleUrls: ['./rank-top.component.css']
+})
+export class RankTopComponent {
+
+}
