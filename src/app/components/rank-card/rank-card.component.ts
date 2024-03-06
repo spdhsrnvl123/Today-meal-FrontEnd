0783dcf3e0b430deb66900295882c0edfac4334a
LeutBing <spdhsrnvl456@gmail.com>
2024-03-06 17:30:23 +0900

    ✨feat:투표순위 리스트 및 투표 1위 컴포넌트 구현

--- src/app/components/rank-card/rank-card.component.ts
+++ src/app/components/rank-card/rank-card.component.ts
@@ -6,9 +6,23 @@ import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
   styleUrls: ['./rank-card.component.css']
 })
 export class RankCardComponent implements OnChanges{
-  @Input() voteData : any;
+  @Input() voteData : any = [];
+  data : any;
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log(this.voteData);
+    let data = this.voteData.sort((a:any,b:any)=>{
+      return  b.vote_count - a.vote_count;
+    })
+
+    //투표 수에 따른 투표순위 속성 값 추가
+    let rank = 1;
+    for(let i = 0; i< data.length; i++){
+      if(i>0 && data[i]['vote_count'] !== data[i-1]['vote_count']){
+        rank++;
+      }
+      data[i]["rank"] = rank;
+    }
+    this.data = data.slice(0,3);
+    console.log(this.data)
   }
 }
--- src/app/layouts/rank-top/rank-top.component.ts
+++ src/app/layouts/rank-top/rank-top.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-rank-top',
   templateUrl: './rank-top.component.html',
   styleUrls: ['./rank-top.component.css']
 })
-export class RankTopComponent {
+export class RankTopComponent implements OnChanges{
+  @Input() voteData : any;
+  highestVoteItem : any
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(this.voteData);
+
+    // vote_count가 가장 높은 항목 찾기
+    let highestVoteItem = this.voteData.reduce((prev:any, current:any) => {
+      return (prev.vote_count > current.vote_count) ? prev : current;
+    });
+    this.highestVoteItem = highestVoteItem;
+    console.log(this.highestVoteItem)
+  }
 }
