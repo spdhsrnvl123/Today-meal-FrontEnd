ea9343df5ea1aa57ebbaca57e9e11648d5cf27e6
LeutBing <spdhsrnvl456@gmail.com>
2024-03-06 17:29:47 +0900

    ✨feat:회원가입페이지 이름작성 유무, 아이디작성 유무, 비밀번호일치 유효성 검사 구현

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -9,12 +9,19 @@ import {Router} from "@angular/router";
   styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent{
-  constructor(private apiService: ApiService, private router:Router) {}
+  constructor(private apiService: ApiService, private router:Router) {
+    this.formData = {
+      // user_id: string,
+    }
+  }
 
   @ViewChild('f') signupForm : NgForm | undefined;
   // joinData  = {};
   duplicationConfirm = false;
   duplicationConfirmReject = false;
+  message: string = '';
+  formData: any = {};
+  // @ts-ignore
 
   onSubmit(){
     console.log(this.signupForm)
@@ -23,17 +30,30 @@ export class SignUpComponent{
       "name" : this.signupForm?.form.value.name,
       "password" : this.signupForm?.form.value.passwordFirst,
     }
-
-    console.log(joinData)
-    this.apiService.joinReq(joinData).subscribe(data =>{
-        if (data == 1){
-          // @ts-ignore
-          // this.router.navigate("/start/success")
-          this.router.navigate(["/start/signin"]);
+    if(this.#isComplete(joinData)) {
+      console.log(joinData)
+      this.apiService.joinReq(joinData).subscribe(data =>{
+          if (data == 1){
+            // @ts-ignore
+            // this.router.navigate("/start/success")
+            this.router.navigate(["/start/signin"]);
+          }
         }
-      }
-    )
+      )
+    }
   }
+
+  #isComplete = (formData: any): any => {
+    console.log(formData.user_id)
+    if(formData.user_id?.length > 3 && formData.name?.length >0 && this.signupForm?.value.passwordFirst == this.signupForm?.value.passwordSecond) {
+      console.log(1)
+      return true;
+    }else{
+      this.message = '다시 입력해주세요.';
+      return false;
+    }
+  }
+
   onIdChange(value: string) {
     console.log(value)
     if(value){
@@ -44,16 +64,19 @@ export class SignUpComponent{
 
   //아이디 중복 검사
   duplicateTest(){
-    let id = this.signupForm?.form.value.id
-    this.apiService.duplicationReq(id).subscribe((data)=>{
-      console.log(data)
-      if(data == 1){
-        this.duplicationConfirm = false;
-        this.duplicationConfirmReject = true;
-      }else{
-        this.duplicationConfirmReject = false;
-        this.duplicationConfirm = true;
-      }
-    })
+    if(this.signupForm?.form.value.id?.length >= 3){
+      this.apiService.duplicationReq(this.signupForm?.form.value.id).subscribe((data)=>{
+        console.log(data)
+        if(data == 1){
+          this.duplicationConfirm = false;
+          this.duplicationConfirmReject = true;
+        }else{
+          this.duplicationConfirmReject = false;
+          this.duplicationConfirm = true;
+        }
+      })
+    }else{
+      console.log("3글자 이상 작성해주세요.")
+    }
   }
 }
